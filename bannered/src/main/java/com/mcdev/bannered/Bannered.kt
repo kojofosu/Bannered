21098d0238dfd31db1a6d523edff4cc8350e9b39
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2022-07-13 05:58:57 +0000

    Fix padding issue. Add title.

--- bannered/src/main/java/com/mcdev/bannered/Bannered.kt
+++ bannered/src/main/java/com/mcdev/bannered/Bannered.kt
@@ -4,14 +4,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.TypedArray
 import android.graphics.Color
-import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
-import androidx.annotation.ColorRes
-import androidx.cardview.widget.CardView
 import androidx.core.content.res.getStringOrThrow
 import com.google.android.material.card.MaterialCardView
 import com.mcdev.bannered.databinding.ActivityBanneredBinding
@@ -29,8 +26,20 @@ class Bannered @JvmOverloads constructor(context: Context,
     private var bannerViewStrokeWidth: Float = 0F
     private var bannerViewStrokeColor: Int? = null
     private var bannerViewBgColor: Int? = null
+    private var title: String? = resources.getString(R.string.app_name)
+    private var titleTextColor: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        resources.getColor(R.color.black, context.theme)
+    } else {
+        Color.parseColor("#2a2a2a")
+    }
+    private var titleTextSize: Float = resources.getDimension(R.dimen.def_text_size)
+    private var titleEnabled : Boolean = true
     private var description: String? = resources.getString(R.string.lorem_ipsum)
-    private var descriptionTextColor: Int = resources.getColor(R.color.black, context.theme)
+    private var descriptionTextColor: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        resources.getColor(R.color.black, context.theme)
+    } else {
+        Color.parseColor("#2a2a2a")
+    }
     private var descriptionTextSize: Float = resources.getDimension(R.dimen.def_text_size)
     private var dismissButtonText: String? = null
     private var dismissButtonEnabled: Boolean = true
@@ -65,15 +74,64 @@ class Bannered @JvmOverloads constructor(context: Context,
     }
 
     private fun getAttributes(attributes: TypedArray) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /*BANNER VIEW*/
+            bannerViewBgColor = attributes.getColor(
+                R.styleable.Bannered_cardBackgroundColor,
+                resources.getColor(R.color.bg_color, context.theme)
+            )
+            bannerViewStrokeColor = attributes.getColor(
+                R.styleable.Bannered_strokeColor,
+                resources.getColor(R.color.black, context.theme)
+            )
+
+            /*TITLE*/
+            titleTextColor = attributes.getColor(
+                R.styleable.Bannered_titleTextColor,
+                resources.getColor(R.color.black, context.theme)
+            )
+
+            /*DESCRIPTION*/
+            descriptionTextColor = attributes.getColor(
+                R.styleable.Bannered_descriptionTextColor,
+                resources.getColor(R.color.black, context.theme)
+            )
+
+        } else {
+            /*BANNER VIEW*/
+            bannerViewBgColor = attributes.getColor(
+                R.styleable.Bannered_cardBackgroundColor,
+                Color.parseColor("#2a2a2a")
+            )
+            bannerViewStrokeColor = attributes.getColor(
+                R.styleable.Bannered_strokeColor,
+                Color.parseColor("#2a2a2a")
+            )
+
+            /*TITLE*/
+            titleTextColor = attributes.getColor(
+                R.styleable.Bannered_titleTextColor,
+                Color.parseColor("#2a2a2a")
+            )
+
+            /*DESCRIPTION*/
+            descriptionTextColor = attributes.getColor(
+                R.styleable.Bannered_descriptionTextColor,
+                Color.parseColor("#2a2a2a")
+            )
+        }
+
         /*Get banner view*/
-        bannerViewBgColor = attributes.getColor(R.styleable.Bannered_cardBackgroundColor, resources.getColor(R.color.bg_color, context.theme))
-        bannerViewStrokeColor = attributes.getColor(R.styleable.Bannered_strokeColor, resources.getColor(R.color.black, context.theme))
         bannerViewStrokeWidth = attributes.getDimension(R.styleable.Bannered_strokeWidth, 0F)
         bannerViewKey = attributes.getStringOrThrow(R.styleable.Bannered_key)
 
+        /*Get title text*/
+        title = attributes.getString(R.styleable.Bannered_title)
+        titleTextSize = attributes.getDimension(R.styleable.Bannered_titleTextSize, resources.getDimension(R.dimen.def_text_size))
+        titleEnabled = attributes.getBoolean(R.styleable.Bannered_titleEnabled, true)
+
         /*Get description text*/
         description = attributes.getString(R.styleable.Bannered_description)
-        descriptionTextColor = attributes.getColor(R.styleable.Bannered_descriptionTextColor, resources.getColor(R.color.black, context.theme))
         descriptionTextSize = attributes.getDimension(R.styleable.Bannered_descriptionTextSize, resources.getDimension(R.dimen.def_text_size))
 
         /*Get dismiss Button text*/
@@ -90,6 +148,18 @@ class Bannered @JvmOverloads constructor(context: Context,
             this.strokeWidth = bannerViewStrokeWidth.toInt()
             this.strokeColor = bannerViewStrokeColor!!
             this.setCardBackgroundColor(bannerViewBgColor!!)
+        }
+
+        /*title*/
+        binding.bannerTitle.apply {
+            if (titleEnabled)
+                this.visibility = View.VISIBLE
+            else
+                this.visibility = View.GONE
+
+            this.text = title
+            this.setTextColor(titleTextColor)
+            this.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleTextSize)
         }
 
         /*description*/
